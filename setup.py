e26541dd0aed6e6b552af751ae5800a6bf774969
sepideh-mofidifar <127847547+sepideh-mofidifar@users.noreply.github.com>
2024-11-23 08:20:50 -0800

    setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,17 @@
 from setuptools import setup, find_packages
 
+# Read the content of README file (ensure it's in the same directory as setup.py)
+with open('README.md', 'r', encoding='utf-8') as f:
+    long_description = f.read()
+
 setup(
     name="PhyloMICOM",  # The name of your package
     version="0.1.0",  # Version of your package
     author="Sepideh Mofidifar",  # Your name or organization
     author_email="s.mofidifar@gmail.com",  # Your email
     description="A package for microbial community analysis",  # Short description of the package
-    long_description=open('README.md').read(),  # Long description read from the README file
+    long_description=long_description,  # Long description read from the README file
+    long_description_content_type="text/markdown",  # Specify the format of long description (markdown in this case)
     url="https://github.com/sepideh-mofidifar/PhyloMICOM",  # URL of the GitHub repository
     packages=find_packages(),  # Automatically find all the sub-packages (like micom, data, analysis)
     install_requires=[  # External dependencies (if any)
